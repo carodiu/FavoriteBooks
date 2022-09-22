d62bec583eae0df63b134cd52b8bb18eeb75077b
Carolina Diaz <net.nimbus@yahoo.com>
2022-09-22 18:02:07 -0400

    Before updating Angular

--- FavoriteBooks/ClientApp/src/app/app.module.ts
+++ FavoriteBooks/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
